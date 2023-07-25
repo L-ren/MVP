1814c65781dfbfe522d69f9079405f33521808bb
Loren <loren.johnson99@yahoo.com>
2023-07-25 09:35:40 -0700

    sets up routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const router = require('./routes.js')
 const app = express();
 
 app.use(morgan('dev'));
+app.use('/', router);
+
+app.listen(process.env.PORT || 3000);
