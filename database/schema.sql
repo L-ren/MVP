9ddbea1761dd4aa9304255bbf28c0d2690e4f2c4
Loren <loren.johnson99@yahoo.com>
2023-07-25 10:15:20 -0700

    sets up database

---
+++ database/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE plants (
+  id INT NOT NULL AUTO-INCREMENT PRIMARY KEY,
+  name,
+  species,
+  waterFreq,
+  sunlight,
+  hardiness,
+  type,
+  waterPeriod,
+  waterVol,
+  maintenance,
+  image
+);
