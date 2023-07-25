bfadafd6382564c611db61fe2ddee0119469a082
Loren <loren.johnson99@yahoo.com>
2023-07-25 09:42:38 -0700

    serves static html page

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const router = require('./routes.js')
 const app = express();
 
 app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/', router);
 
 app.listen(process.env.PORT || 3000);
