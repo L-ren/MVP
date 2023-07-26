844a340b2d6423a9974a813a6a641c46e603551c
Loren <loren.johnson99@yahoo.com>
2023-07-26 13:48:48 -0700

    WEBPACK WORKS

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-() => console.log('webpack works');
-ReactDOM.render(
-  React.createElement('div', null, `Hello React`),
-  document.getElementById('root')
-)
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+// ReactDOM.render(
+//   React.createElement('div', null, `Hello React`),
+//   document.getElementById('root')
+// )
