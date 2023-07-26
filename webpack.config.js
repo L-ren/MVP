b30263158b157e1d858a8c9b7972a8dc7f0b7729
Loren <loren.johnson99@yahoo.com>
2023-07-26 13:48:48 -0700

    WEBPACK WORKS

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,71 @@
-const path = require('path')
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'client/src', 'index.js'),
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
-    path: path.resolve(__dirname, 'client/dist'),
-    filename: 'bundle.js'
+    filename: "bundle.js",
+    path: DIST_DIR
   },
   module: {
     rules: [
       {
-        test: /\.(jsx|js)$/,
-        include: path.resolve(__dirname, 'src'),
+        test: [/\.jsx$/],
         exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
+        use:{
+          loader: "babel-loader",
           options: {
-            presets: [
-              ['@babel/preset-env', {
-                "targets": "defaults"
-              }],
-              '@babel/preset-react'
-            ]
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           }
-        }]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader"}
+        ]
       }
     ]
   }
-}
+};
+
+// const path = require('path')
+
+// module.exports = {
+//   mode: 'development',
+//   entry: path.resolve(__dirname, 'client/src', 'index.js'),
+//   output: {
+//     path: path.resolve(__dirname, 'client/dist'),
+//     filename: 'bundle.js'
+//   },
+//   devServer: {
+//     static: path.resolve(__dirname, 'client/dist'),
+//     open: true,
+//     client: {
+//       logging: 'none'
+//     },
+//     port: 9000
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(jsx|js)$/,
+//         include: path.resolve(__dirname, 'src'),
+//         exclude: /node_modules/,
+//         use: [{
+//           loader: 'babel-loader',
+//           options: {
+//             presets: [
+//               '@babel/preset-env',
+//               '@babel/preset-react'
+//             ]
+//           }
+//         }]
+//       }
+//     ]
+//   }
+// }
