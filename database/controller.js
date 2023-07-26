14e3bd145ae50b2ef0959b77b94a7f3a338c863e
Loren <loren.johnson99@yahoo.com>
2023-07-26 16:11:20 -0700

    before promisifying queries

--- database/controller.js
+++ database/controller.js
@@ -3,6 +3,7 @@ const model = require('./model.js'); // creates tables if not already in db
 
 module.exports.getProfiles = {};
 
-module.exports.createProfile = () => {
-  console.log('request received');
+module.exports.createProfile = (plantId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) => {
+  let values = [plantId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance];
+  return db.query(`INSERT INTO PLANTS (perenualId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) VALUES (?)`, [values])
 };
