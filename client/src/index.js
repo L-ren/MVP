121438eb305475ac6a4b7a6a99f2b7b078542638
Loren <loren.johnson99@yahoo.com>
2023-07-26 13:55:22 -0700

    click handlers respond

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+// import React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// import React from 'react'
-// import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// ReactDOM.render(
-//   React.createElement('div', null, `Hello React`),
-//   document.getElementById('root')
-// )
+ReactDOM.render(
+  React.createElement('div', null, `Hello React`),
+  document.getElementById('root')
+)
