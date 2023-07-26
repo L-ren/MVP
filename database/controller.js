05493bec4352d518691517fb10af94fb49e859ad
Loren <loren.johnson99@yahoo.com>
2023-07-26 16:56:43 -0700

    gets plant data on first load

--- database/controller.js
+++ database/controller.js
@@ -1,10 +1,13 @@
 const db = require('./index.js');
 const model = require('./model.js'); // creates tables if not already in db
 
-module.exports.getProfiles = {};
+module.exports.getPlants = () => {
+  console.log(`received`);
+  return db.query(`SELECT * FROM plants`)
+};
 
 module.exports.createProfile = (plantId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) => {
   let values = [plantId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance];
 
-  return db.query(`INSERT INTO PLANTS (perenualId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) VALUES (?)`, [values])
+  return db.query(`INSERT INTO plants (perenualId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) VALUES (?)`, [values])
 };
