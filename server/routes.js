c43190c82bb7b15f685e3ddc9a8089f53cdfd706
Loren <loren.johnson99@yahoo.com>
2023-07-26 15:44:44 -0700

    gets plant info from API

--- server/routes.js
+++ server/routes.js
@@ -8,11 +8,26 @@ router.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  axios.get(`https://perenual.com/api/species-list?page=1&key=sk-nEde64bf2def48cd51665&q=${req.body.species}`)
-  .then((res) => console.log(res.data))
-  .catch((err) => console.log(err));
+router.post('/', async (req, res) => {
+  const name = req.body.name;
+  const species = req.body.species;
+
+  const plantInfo = await axios.get(`https://perenual.com/api/species-list?page=1&key=sk-nEde64bf2def48cd51665&q=${species}`)
+
+  const plantId = plantInfo.data.data[0].id;
+  const sunlight = plantInfo.data.data[0].sunlight;
+  const waterFreq = plantInfo.data.data[0].watering;
+
+  const morePlantInfo = await axios.get(`https://perenual.com/api/species/details/${[plantId]}?key=sk-nEde64bf2def48cd51665`)
+
+  const type = morePlantInfo.data.type;
+  const hardiness = morePlantInfo.data.hardiness;
+  const waterPeriod = morePlantInfo.data.watering_period;
+  const waterDepth = morePlantInfo.data.depth_water_requirement;
+  const maintenance = morePlantInfo.data.maintenance;
+
+  console.log(sunlight, waterFreq, type, hardiness, waterPeriod, waterDepth, maintenance);
+  // create plant profile, pass in
   // controller.createProfile();
   res.sendStatus(201);
 })
