7976b89a67ba0dd1dc32a140e5c15f3d19ea8e4f
Loren <loren.johnson99@yahoo.com>
2023-07-26 17:29:26 -0700

    ready to test arduino wifi

--- client/src/components/PlantList.jsx
+++ client/src/components/PlantList.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PlantTile from './PlantTile.jsx';
 
-const PlantList = () => {
-  return (
-    <div>
-      <h3>My plants</h3>
-      <PlantTile />
-      <PlantTile />
-    </div>
-  );
+const PlantList = ({ myPlants }) => {
+  console.log(myPlants)
+  if (Array.isArray(myPlants)) {
+    return (
+      <div>
+        <h3>My plants</h3>
+        {myPlants.map(plant => <PlantTile plant={plant} />)}
+      </div>
+    );
+  }
+  return;
 };
 
 export default PlantList;
--- client/src/components/PlantTile.jsx
+++ client/src/components/PlantTile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const PlantTile = () => {
+const PlantTile = ({ plant }) => {
   const onEdit = (e) => {
     console.log('editing!');
   }
@@ -8,15 +8,15 @@ const PlantTile = () => {
     console.log('deleting!');
   }
 
+  console.log(plant);
   return (
     <div className='tile'>
-      Name
-      Species
-      Age
+      <h4>{plant.name}</h4>
+      {plant.species}
       current conditions
       status update
-      <button onClick={onEdit}>edit</button>
-      <button onClick={onDelete}>delete</button>
+      {/* <button onClick={onEdit}>edit</button>
+      <button onClick={onDelete}>delete</button> */}
     </div>
   );
 };
