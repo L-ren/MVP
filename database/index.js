c3f45793cffeb6924c15871f09dbbcad381a6d43
Loren <loren.johnson99@yahoo.com>
2023-08-18 17:23:25 -0700

    adding new plant creates initial entry in sensorData db table

--- database/index.js
+++ database/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'myplants'
+  database: 'myplants',
+  multipleStatements: true, // vulnerable to SQL injection, make sure inputs are escaped
 });
 
 module.exports = pool;
