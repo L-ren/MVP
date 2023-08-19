d2bd5c0674c3374ceb62cf1e48ac50f2b3c8c245
Loren <loren.johnson99@yahoo.com>
2023-08-18 17:23:25 -0700

    adding new plant creates initial entry in sensorData db table

--- database/controller.js
+++ database/controller.js
@@ -5,13 +5,13 @@ module.exports.getPlants = () => {
   console.log(`received`);
   // get most recent plant sensor data
   // return db.query(`SELECT * FROM plants`); // get info on ALL plants (inc. those without sensor data)
-  return db.query(`SELECT plants.id, name, species, waterFreq, sunlight, hardiness, waterDepth, maintenance, time, temp, humidity, light, moisture FROM plants INNER JOIN sensorData ON plants.id=sensorData.plantId`)
+  return db.query(`SELECT plants.id, name, species, waterFreq, sunlight, hardiness, waterDepth, maintenance, time, temp, humidity, light, moisture FROM plants INNER JOIN sensorData ON plants.id=sensorData.plantId WHERE time=(SELECT MAX(time))`)
 };
 
 module.exports.createProfile = (plantId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) => {
   let values = [plantId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance];
-
-  return db.query(`INSERT INTO plants (perenualId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) VALUES (?)`, [values])
+  let date = new Date();
+  return db.query(`INSERT INTO plants (perenualId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) VALUES (?); INSERT INTO sensorData (plantId, time) VALUES (LAST_INSERT_ID(), '${date.toISOString().slice(0, 19).replace('T', ' ')}');`, [values])
 };
 
 module.exports.editProfile = (id, plantId, name, species, waterFreq, sunlight, hardiness, type, waterPeriod, waterDepth, maintenance) => {
@@ -24,6 +24,7 @@ module.exports.deleteProfile = (id) => {
 };
 
 module.exports.saveSensorData = ({id, temp, humidity, light, moisture}) => {
-  const date = new Date();
+  const rawDate = new Date();
+  const date = rawDate.toISOString().slice(0, 19).replace('T', ' ');
   return db.query(`INSERT INTO sensorData (plantId, time, temp, humidity, light, moisture) VALUES (?)`, [[id, date, temp, humidity, light, moisture]]);
 }
