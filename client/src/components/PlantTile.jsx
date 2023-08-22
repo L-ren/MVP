76a7a9b6631094157b4f7dfb3e5363193617c19f
Loren <loren.johnson99@yahoo.com>
2023-08-22 11:28:23 -0700

    displays time and date of latest sensor data in readable format

--- client/src/components/PlantTile.jsx
+++ client/src/components/PlantTile.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import dayjs from 'dayjs';
+import './PlantTile.css'
 
 const PlantTile = ({ plant, myPlants, setMyPlants, setEditPlant }) => {
   const onEdit = (e) => {
@@ -18,23 +20,29 @@ const PlantTile = ({ plant, myPlants, setMyPlants, setEditPlant }) => {
     });
 
     setMyPlants(myPlants.filter(plant => (plant.id !== parseInt(id))));
+    let dayjsTime = dayjs(plant.time);
   }
 
   return (
     <div className='tile'>
       <h4>{plant.name}</h4>
       <h5>{plant.species}</h5>
-      <div className='staticData'>
-        <span>Sunlight: {plant.sunlight}</span>
-        <span>Water Frequency: {plant.waterFreq}</span>
-        <span>Maintenance: {plant.maintenance}</span>
-      </div>
-      <div className='sensorData'>
-        <h5>Current conditions</h5>
-        <span>Temperature: {plant.temp}</span>
-        <span>Humidity: {plant.humidity}</span>
-        <span>Light: {plant.light}</span>
-        <span>Soil Moisture: {plant.moisture}</span>
+      <div className='tileData'>
+        <div className='staticData'>
+          <span>Sunlight: {plant.sunlight}</span>
+          <span>Water Frequency: {plant.waterFreq}</span>
+          <span>Maintenance: {plant.maintenance}</span>
+        </div>
+        {/* only show live data if available */}
+        { (plant.temp && plant.light && plant.moisture) &&
+        <div className='sensorData'>
+          {/* <h5>Current conditions</h5> */}
+          <span>Temperature: {plant.temp}</span>
+          <span>Humidity: {plant.humidity}</span>
+          <span>Light: {plant.light}</span>
+          <span>Soil Moisture: {plant.moisture}</span>
+          <span className='timeStamp'>Conditions as of {dayjs(plant.time).format('dddd, MMMM D, YYYY[ at ]HH:mm')}</span>
+        </div>}
       </div>
       <div className = "tileButtons">
         <button onClick={onEdit} className={plant.id}>edit</button>
