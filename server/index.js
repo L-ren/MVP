a6bdb322e8197629012f10920408aa639a598b13
Loren <loren.johnson99@yahoo.com>
2023-08-24 14:54:02 -0700

    changed api key, stored in env

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const router = require('./routes.js')
 const app = express();
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
@@ -12,5 +13,6 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/', router);
 
+console.log(process.env.PORT)
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
