afc8ea56151a092df5e11d4ed9f4f64ef674a4f6
Ubuntu <ubuntu@ip-172-31-16-175.us-east-2.compute.internal>
2023-10-09 22:04:15 +0000

    this branch is for the app running the database on an ec2 instance

--- database/index.js
+++ database/index.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2/promise');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'myplants',
+  password: process.env.DB_PW,
   multipleStatements: true, // vulnerable to SQL injection, make sure inputs are escaped
 });
 
